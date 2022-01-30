13a4d0afe72567b08e60e1adf19b1042677fd777
namgay tenzin <namgay.tenzin@dcpl.bt>
2022-01-30 18:31:01 +0600

    change application title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Footer from "./components/footer";
 import Hero from "./components/hero";
 import MainSection from "./components/mainSection";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="font-sans">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Bhutan Notifications</title>
+      </Helmet>
       <div className="px-0 py-5 md:px-10 md:py-10 lg:px-40 lg:py-10">
         <Hero />
         <MainSection />
